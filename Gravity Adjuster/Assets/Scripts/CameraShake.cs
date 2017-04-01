fba777cb39baf3534473b4a0d433e720cd66cf4b
Saorwen <lynchsaoirse@gmail.com>
2017-04-01 16:21:07 +0100

    Screen Shake
    Moved objects in scene forward on z-axis, so that camera starts at world position zero. Created cameraShake script. Initial area in which the camera move is determined by a global float. added line "cameraShake.shakeAmount = 2f" to Update() in grenade script. Throw some grenades and see how it looks!

---
+++ Gravity Adjuster/Assets/Scripts/CameraShake.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour {
+
+	public static float ShakeAmount = 0f;
+
+	public bool shaking = false;
+	public float shakeDecay = 0.8f;
+	Vector3 originPos;
+	Quaternion originRot;
+
+	// Use this for initialization
+	void Start () {
+		originPos = gameObject.transform.position;
+		originRot = gameObject.transform.localRotation;
+		if (shaking == true) {
+			ShakeAmount = 10f;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (ShakeAmount > 0) {
+			Vector3 moveAmount = Random.insideUnitCircle * ShakeAmount;
+			moveAmount.z = 0;
+			ShakeAmount = Mathf.Lerp (ShakeAmount, 0, shakeDecay);
+		//	ShakeAmount -= shakeDecay;
+			transform.position = moveAmount;
+		} else {
+			gameObject.transform.position = originPos;
+		}
+	}
+}
